e5d1a746e5043dd1d4d9d2c7974d035497f7ae8a
nick-ross <mx5433@gmail.com>
2014-08-18 14:47:34 -0500

    Initial Commit

---
+++ swift-test/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-test
+//
+//  Created by Nick Ross on 8/18/14.
+//  Copyright (c) 2014 Nick Ross. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
